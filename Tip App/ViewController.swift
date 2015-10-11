408b5d6aa0ed4ee6edee18d116572033ef613106
Benji Renzo Kuroda <benjikuroda@benjikuroda-mbp.local>
2015-10-10 22:21:58 -0700

    Initial Commit

---
+++ Tip App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip App
+//
+//  Created by Benji Renzo Kuroda on 10/10/15.
+//  Copyright © 2015 Benji Renzo Kuroda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
