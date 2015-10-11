26c1a13b4371305ffe61d06234fb83b4582e0da2
benjirenzo <benjirenzo@gmail.com>
2015-10-11 14:34:42 -0700

    Tip Calc with Bonus
    Tip calculator with radios to toggle percentage. Bonus stepper to
    toggle number of people in party. Still working on the last feature may
    need help debugging it.

--- Tip App/ViewController.swift
+++ Tip App/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet var totalPerPerson: UILabel!
+    @IBOutlet var peopleLabel: UILabel!
+    @IBOutlet var peopleStepper: UIStepper!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        totalPerPerson.text = "$0.00"
+        peopleStepper.wraps = false
+        peopleStepper.autorepeat = true
+        peopleStepper.maximumValue = 10
+        peopleLabel.text = ("1")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func stepperValueChanged(sender: UIStepper) {
+        peopleLabel.text = Int(sender.value).description
+    }
 
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.20, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var peopleNumber = NSString(string: peopleLabel.text!).doubleValue
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        var totalPerson = total / peopleNumber
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        totalPerPerson.text = String(format: "$%.2f", totalPerson)
+        
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
